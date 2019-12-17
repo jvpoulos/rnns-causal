1e8d642bb449e1a040c2276cb2885d64e25f3dc0
Jason Poulos <jvpoulos@gmail.com>
2019-12-17 12:30:08 -0500

    db

--- code/train_encoder_decoder_sim.py
+++ code/train_encoder_decoder_sim.py
@@ -83,7 +83,7 @@ def test_model():
     dXC, dYC = [], []
     for i in range(seq_len-n_pre):
         dXC.append(x_scaled[i:i+n_pre]) # controls are inputs
-        dYC.append(x_scaled[i+n_pre:i+n_pre+n_post]) # controls are outputs
+        dYC.append(x_scaled[i+n_pre:i+n_pre]) # controls are outputs
     
     dataXC = np.array(dXC)
     dataYC = np.array(dYC)
