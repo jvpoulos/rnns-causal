3679c1f3414b985839b2e5a2b3d99115556b2ece
Jason Poulos <jvpoulos@gmail.com>
2020-04-02 18:54:15 -0400

    db

---
+++ code/placebo-results-table.R
@@ -0,0 +1,34 @@
+### Load placebo test results ###
+
+# Basque Country
+basque_N_16_T_43_numruns_100_num_treated_8_simultaneuous_1 <- 
+  load("results/table-results/basque_N_16_T_43_numruns_100_num_treated_8_simultaneuous_1.rds")
+
+df1$stderror <- (df1$y -df1$lb)/1.96
+
+print(df1, digits=2)
+
+# California smoking
+california_N_38_T_31_numruns_100_num_treated_19_simultaneuous_1 <- 
+  load("results/table-results/california_N_38_T_31_numruns_100_num_treated_19_simultaneuous_1.rds")
+
+df1$stderror <- (df1$y -df1$lb)/1.96
+
+print(df1, digits=4)
+
+# W. German reunification
+germany_N_16_T_44_numruns_100_num_treated_8_simultaneuous_1 <- 
+  load("results/table-results/germany_N_16_T_44_numruns_100_num_treated_8_simultaneuous_1.rds")
+
+df1$stderror <- (df1$y -df1$lb)/1.96
+
+print(df1, digits=4)
+
+# Education spending
+
+educ_pc_N_16_T_156_numruns_100_num_treated_8_simultaneuous_1 <- 
+  load("/media/jason/Dropbox/github/rnns-causal/results/table-results/educ_pc_N_16_T_156_numruns_100_num_treated_8_simultaneuous_1.rds")
+
+df1$stderror <- (df1$y -df1$lb)/1.96
+
+print(df1, digits=4)
