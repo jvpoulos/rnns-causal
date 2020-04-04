6243c92e0bce27864e96c45519a8d970aabfb1b1
Jason Poulos <jvpoulos@gmail.com>
2020-04-04 10:54:37 -0400

    lower N dimension sales

--- code/placebo-results-table.R
+++ code/placebo-results-table.R
@@ -27,8 +27,24 @@ print(df1, digits=4)
 # Education spending
 
 educ_pc_N_16_T_156_numruns_100_num_treated_8_simultaneuous_1 <- 
-  load("/media/jason/Dropbox/github/rnns-causal/results/table-results/educ_pc_N_16_T_156_numruns_100_num_treated_8_simultaneuous_1.rds")
+  load("results/table-results/educ_pc_N_16_T_156_numruns_100_num_treated_8_simultaneuous_1.rds")
 
 df1$stderror <- (df1$y -df1$lb)/1.96
 
-print(df1, digits=4)
+print(df1, digits=2)
+
+# Mayoral elections
+
+votediff_N_910_T_66_numruns_100_num_treated_455_simultaneuous_1 <- 
+  load("results/table-results/votediff_N_910_T_66_numruns_100_num_treated_455_simultaneuous_1.rds")
+
+df1$stderror <- (df1$y -df1$lb)/1.96
+
+print(df1, digits=3)
+
+# Covid-19
+covid19 <- readRDS("results/table-results/covid_N_1000_T_60_numruns_100_num_treated_500_simultaneuous_1.rds")
+
+covid19$stderror <- (covid19$y -covid19$lb)/1.96
+
+print(covid19*100, digits=2)
