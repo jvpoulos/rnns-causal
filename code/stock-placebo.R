98a3c2fe2bea6be09684659d2d752536148a7995
Jason Poulos <jvpoulos@gmail.com>
2020-04-06 11:49:36 -0400

    rm covid, elections, increase T for sales and stock

--- code/stock-placebo.R
+++ code/stock-placebo.R
@@ -213,4 +213,4 @@ Y <- t(read.csv('data/returns_no_missing.csv',header=F)) # N X T
 
 print(paste0("N X T data dimension: ", dim(Y)))
 
-StockSim(Y,N=100,T=1000,sim=1)
+StockSim(Y,N=100,T=1500,sim=1)
